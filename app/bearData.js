782e3f57f29174226c6fcadb7603f67b0739b930
cch5ng <cch5ng@gmail.com>
2016-07-04 13:44:48 -0700

    chore: init files

---
+++ app/bearData.js
@@ -0,0 +1,59 @@
+var data = [
+	{	id: '',
+		number: 814,
+		name: 'Lurch',
+		picUrls: [],
+		desc: 'Lurch is missing his right year. He is one of the dominant males at Brooks Falls and frequently displaces females and subordinant males from their fishing spots.',
+		earMissing: true
+	},
+	{	id: '',
+		number: 480,
+		name: 'Otis',
+		picUrls: [],
+		desc: 'Otis is one of the seniors at Brooks Falls and a crowd favorite. He has a floppy right ear. He often fishes the back office (the far bank opposite the cams and close to the falls). When the jacuzzi is vacant, he will move over there. He does not compete with the 3 most dominant males and will give up his fishing spot to them.',
+		earFloppy: true
+	},
+	{	id: '',
+		number: 83,
+		name: 'Wayne Brother',
+		picUrls: [],
+		desc: 'Wayne Brother is the surviving brother of two. He is named after Bruce Wayne for his batman-shaped ears. He is a frequent Brooks Falls fisher with recognizable scars on his upper left shoulder and on his back.'
+	},
+	{	id: '',
+		number: 128,
+		name: 'Grazer',
+		picUrls: [],
+		desc: 'Grazer has 3 cubs in 2016 and she is a first-time mother. She is a uniformly blond colored bear. She and her family have been seen more towards the Lower River cam. In 2015, before having cubs, Grazer typically fished from the Lip of the falls.',
+		cubs: 3
+	},
+	{	id: '',
+		number: 409,
+		name: 'Beadnose',
+		picUrls: [],
+		desc: 'Beadnose has 2 cubs in 2016. This is her fourth known litter. Her cubs have dark colored fur. During 2015, Beadnose frequently fished from the Lip of the falls, delicately balanced at the edge. In 2016 she occasionally still fishes the Lip but later a night.',
+		cubs: 2
+	},
+	{	id: '',
+		number: 284,
+		name: 'Electra',
+		picUrls: [],
+		desc: 'Electra has 2 cubs in 2016. This is her first known litter. Her cubs have dark colored fur. In the Katmai bear book, she does not have an official name but she was nick-named after Amelia Earhart"s plane because 284"s mother is Amelia.',
+		cubs: 2
+	},
+	{	id: '',
+		number: 402,
+		name: null,
+		picUrls: [],
+		desc: 'Bear 402 has 2 cubs in 2016. She had 4 cubs from this litter in late 2015 but 2 are remaining now. Her cubs a relatively larger than the other sow"s cubs and are light colored.',
+		cubs: 2
+	},
+	{	id: '',
+		number: 273,
+		name: 'Velma',
+		picUrls: [],
+		desc: 'Bear 273 is the mother of Velcro. 273 has a heart-shaped head and both she and Velcro are lighter colored bears. She does not have an official name in the Katmai bear book but was nicknamed as the "ma of Velcro".',
+		cubs: 1
+	}
+
+
+]
---
+++ app/components/App.jsx
@@ -0,0 +1,265 @@
+//app/components/App.jsx
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import Bear from './Bear.jsx';
+//import BearList from './BearList.jsx';
+//import NavLink from './NavLink.js';
+
+export default class App extends React.Component {
+	constructor(props) {
+		super();
+		// let namesAr = [];
+		// this.state = {
+		// 	recipes: [],
+		// 	show: false,
+		// 	nameValid: 'success'
+		// };
+	}
+
+	// componentWillMount() {
+	// }
+
+	render() {
+		var bearNodes = this.props.data.map(function(bear) {
+			return (
+				<div>
+					<h3>Bear {bear.number} ({bear.name})</h3>
+					<p>{bear.desc}</p>
+				</div>
+			)
+		})
+
+
+		return (
+			<div className="container-fluid" data={this.props.data}>
+				<div className="row">
+					<header>
+						<nav className="navbar navbar-default">
+							<div className="container-fluid">
+								<div className="navbar-header">
+									<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+										<span className="sr-only">Toggle navigation</span>
+										<span className="icon-bar"></span>
+										<span className="icon-bar"></span>
+										<span className="icon-bar"></span>
+									</button>
+									<a href='/' className="navbar-brand header-link" activeClassName='active'>Bear ID Helper</a>
+								</div>
+							</div>{/* /.container-fluid */}
+						</nav>
+					</header>
+				</div>
+
+				<main>
+					<form>
+					    <details>
+					      <summary>Ears</summary>
+					      <fieldset>
+					        <input type="radio" name="ear-missing" id="ear-missing" value="missing_ear" />Missing right ear
+					        <input type="radio" name="ear-floppy" id="ear-floppy" value="floppy_ear" />Floppy right ear
+					      </fieldset>
+					    </details>
+					    <details>
+					      <summary>Scars</summary>
+					      <fieldset>
+					        <input type="radio" name="shoulder-left-scar" id="shoulder-left-scar" value="shoulder-left-scar" />Upper left shoulder and back
+					      </fieldset>
+					    </details>
+					    <details>
+					      <summary>Cubs</summary>
+					      <fieldset>
+					        <input type="radio" name="three-cubs" id="three-cubs" value="three-cubs" />3 cubs
+					        <input type="radio" name="two-cubs" id="two-cubs" value="two-cubs" />2 cubs
+					        <input type="radio" name="one-cub" id="one-cub" value="one-cub" />1 cub
+					      </fieldset>
+					    </details>
+					    {/* not sure how to handle at the moment
+					    <details>
+					      <summary>Fishing Spot</summary>
+					      <fieldset>
+					        <input type="radio" name="far-bank-bf" id="far-bank-bf" value="far-bank-bf">Far Banks of Brooks Falls
+					        <input type="radio" name="jacuzzi" id="jacuzzi" value="jacuzzi">Jacuzzi
+					        <input type="radio" name="lip" id="lip" value="lip">The Lip
+					        <input type="radio" name="riffles" id="riffles" value="riffles">The Riffles
+					      </fieldset>
+					    </details>
+					    */}
+					    <details>
+					      <summary>Courting Bears</summary>
+					      <fieldset>
+					        <input type="radio" name="four-ton-747" id="four-ton-747" value="four-ton-747" />747 and 410
+					      </fieldset>
+					    </details>
+					    <details>
+					      <summary>Fur Color</summary>
+					      <fieldset>
+					        <input type="radio" name="light-fur" id="light-fur" value="light-fur" />Light fur color
+					        {/* <input type="radio" name="dark-fur" id="dark-fur" value="dark-fur" />Dark fur color
+					        */}
+					      </fieldset>
+					    </details>
+					        <button type="button" name="filterBtn" className="btn">Filter Bears</button>
+					        <button type="button" name="clearBtn" className="btn">Clear</button>
+					        <button type="button" name="allBtn" className="btn">All</button>
+					</form>
+				</main>
+
+				{/* bear data section */}
+				{bearNodes}
+
+				<footer className="row footer">
+					<div className="col-xs-12 col-sm-12">
+						<p className="text-center">With <i className="fa fa-heart" aria-hidden="true"></i> from<br />
+							Bear 410's kisser!
+						</p>
+						<p className="text-center">
+							<a href="http://explore.org/live-cams/player/brown-bear-salmon-cam-brooks-falls" target="_blank">May the Furs be With You!</a><br />
+							<a href="https://github.com/cch5ng/" target="_blank">Source</a>
+						</p>
+					</div>
+				</footer>
+
+			</div>
+		);
+	}
+
+	/**
+	 * Adds recipe to local storage and to state
+	 * @param  {[type]} event [description]
+	 * result: update firebase
+	 */
+	// addRecipe = (event) => {
+	// 	if (this.state.nameValid === 'success') {
+	// 		let name = document.getElementById('recipeName').value;
+	// 		event.preventDefault();
+
+	// 		//parsing the ingredients, cleaning up the format so it will display cleanly later on
+	// 		var ingredientsStr = document.getElementById('recipeIngredients').value;
+	// 		var ingredientsAr = ingredientsStr.split(',');
+	// 		//stores final array of ingredients strings, trimmed
+	// 		var ingredientsTrim = [];
+	// 		ingredientsAr.forEach(function(item) {
+	// 			var itemCopy = item.slice(0).trim();
+	// 			ingredientsTrim.push(itemCopy);
+	// 		});
+
+	// 		var stepsStr = document.getElementById('recipeSteps').value;
+	// 		var stepsAr = stepsStr.split('\n');
+
+	// 		//authenticate session before insert
+	// 		let authData = base.getAuth();
+	// 		if (authData) {
+	// 			base.push('recipes', {
+	// 				data:  {name: name,
+	// 					owner: 'cchung',
+	// 					ingredients: ingredientsTrim,
+	// 					steps: stepsAr
+	// 				},
+	// 				then(){
+	// 					//console.log('inserted recipe');
+	// 				}
+	// 			});
+	// 		} else {
+	// 			base.authWithOAuthPopup('google', (error, authData) => {
+	// 				if (error) {
+	// 					console.log("Login Failed!", error);
+	// 				} else {
+	// 					console.log("Authenticated successfully with payload");
+	// 					//update firebase
+	// 				}
+	// 			}, {
+	// 				remember: 'sessionOnly'
+	// 			});
+	// 		}
+	// 	}
+	// };
+
+	/**
+	 * Form validation to ensure that a new name field is unique (key in localStorage)
+	 * @param  {[type]} event [description]
+	 * 
+	 */
+	// validationState = (event) => {
+	// 	this.setState({
+	// 		nameValid: 'success'
+	// 	})
+	// 	let curName = event.target.value;
+	// 	let namesAr = [];
+	// 	let recipesAr = this.state.recipes;
+	// 	let keysAr = [];
+	// 	recipesAr.map(function(recipe) {
+	// 		if (recipe.name === curName) {
+	// 			namesAr.push(recipe.name);
+	// 		}
+	// 	});
+
+	// 	for (let i = 0; i < namesAr.length; i++) {
+	// 		if (curName === namesAr[i]) {
+	// 			this.setState({nameValid: 'error'});
+	// 		} else {
+	// 			this.setState({nameValid: 'success'});
+	// 		}
+	// 	}
+	// };
+
+	/**
+	 * Deletes a recipe from localStorage and state (recipes array). Triggered from Recipe.jsx button.
+	 * @param  {String} id - Recipe id
+	 * @result: update firebase
+	 */
+	// deleteRecipe = (key) => {
+	// 	var onComplete = function(error) {
+	// 		if (error) {
+	// 			console.log('synchronization issue: ' + error);
+	// 		} else {
+	// 		}
+	// 	}
+
+	// 	//authenticate session before insert
+	// 	let authData = recipesRef.getAuth();
+	// 	if (authData) {
+	// 		//update firebase
+	// 		recipesRef.child(key).remove(onComplete);
+	// 	} else {
+	// 		recipesRef.authWithOAuthPopup('google', (error, authData) => {
+	// 			if (error) {
+	// 				console.log("Login Failed!", error);
+	// 			} else {
+	// 				console.log("Authenticated successfully with payload");
+	// 			}
+	// 		}, {
+	// 			remember: 'sessionOnly'
+	// 		});
+	// 	}
+	// };
+
+	/**
+	 * Triggers rebase authWithOAuthPopup() function for google-based login.
+	 * @param  {String} id - Recipe id
+	 * result: log into both Google and firebase
+	 */
+	// googleLogin = () => {
+	// 	base.authWithOAuthPopup('google', (error, authData) => {
+	// 		if (error) {
+	// 			console.log("Login Failed!", error);
+	// 		} else {
+	// 			console.log("Authenticated successfully with payload");
+	// 		}
+	// 	}, {
+	// 		remember: 'sessionOnly'
+	// 	});
+	// }
+
+	/**
+	 * Log out of firebase
+	 * @param  {String} id - Recipe id
+	 * result: log out of firebase but does not log out of Google acct
+	 */
+	// logOut = () => {
+	// 	base.unauth();
+	// 	if (recipesRef.getAuth()) {
+	// 		recipesRef.unauth();
+	// 	}
+	// }
+}
