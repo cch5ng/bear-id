2283789d7042823d6cb50075a1ffc1a2267b823b
cch5ng <cch5ng@gmail.com>
2016-07-09 00:33:47 -0700

    content: add nav

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -47,12 +47,52 @@ export default class App extends React.Component {
 				<div className="row">
 					<header>
 						<nav className="navbar navbar-default">
+						  <div className="container-fluid">
+						    {/*} Brand and toggle get grouped for better mobile display */}
+						    <div className="navbar-header">
+						      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						        <span className="sr-only">Toggle navigation</span>
+						        <span className="icon-bar"></span>
+						        <span className="icon-bar"></span>
+						        <span className="icon-bar"></span>
+						      </button>
+						      <a href='/' className="navbar-brand header-link" >Bear ID Helper</a>{/* activeClassName='active'*/}
+						    </div>
+
+						    {/*} Collect the nav links, forms, and other content for toggling */}
+						    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+						      <ul className="nav navbar-nav navbar-right">
+						          <li><a href="#find">Find a Bear</a></li>
+						          <li><a href="#bears">Bears</a></li>
+						          <li><a href="#map">Brooks Falls</a></li>
+						      </ul>
+						    </div>{/*} /.navbar-collapse */}
+						  </div>{/*} /.container-fluid */}
+						</nav>
+
+
+						{/*<nav className="navbar navbar-default">
 							<div className="container-fluid">
 								<div className="navbar-header">
+									<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+										<span className="sr-only">Toggle navigation</span>
+										<span className="icon-bar"></span>
+										<span className="icon-bar"></span>
+										<span className="icon-bar"></span>
+									</button>
 									<a href='/' className="navbar-brand header-link" activeClassName='active'>Bear ID Helper</a>
 								</div>
+
+								<div className="collapse navbar-collapse" id="">
+									<ul className="nav navbar-nav navbar-right">
+										<li><a href="#find">Find a Bear</a></li>
+										<li><a href="#bears">Bears</a></li>
+										<li><a href="#map">Brooks Falls</a></li>
+									</ul>
+								</div>
 							</div>
-						</nav>
+						</nav>*/}
 					</header>
 				</div>
 
